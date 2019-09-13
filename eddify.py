a3bf47a3b2110e6c76e562a26da350b42f6eb511
Dakota Frost <37204445+icicl@users.noreply.github.com>
2019-09-12 22:54:25 -0700

    added all builtin functions/keywords

--- eddify.py
+++ eddify.py
@@ -1,7 +1,10 @@
 import os
+import keyword
 extrawack = True
+builtin_f = ['abs', 'dict', 'help', 'min', 'setattr', 'all', 'dir', 'hex', 'next', 'slice', 'any', 'divmod', 'id', 'object', 'sorted', 'ascii', 'enumerate', 'input', 'oct', 'staticmethod', 'bin', 'eval', 'int', 'open', 'str', 'bool', 'exec', 'isinstance', 'ord', 'sum', 'bytearray', 'filter', 'issubclass', 'pow', 'super', 'bytes', 'float', 'iter', 'print', 'tuple', 'callable', 'format', 'len', 'property', 'type', 'chr', 'frozenset', 'list', 'range', 'vars', 'classmethod', 'getattr', 'locals', 'repr', 'zip', 'compile', 'globals', 'map', 'reversed', '__import__', 'complex', 'hasattr', 'max', 'round', 'delattr', 'hash', 'memoryview', 'set']
+keywords = keyword.kwlist + builtin_f
+
 file = str(input("File to Eddyify:"))
-keywords = {"True","False","input","range","if","elif","else","open","for","while","with","break","print","input","as","from","len","str","dict","int","def","in","return","and","or","not","global","ord","chr"}
 remap = dict()
 c = 1
 def rewrite(s,syms):
